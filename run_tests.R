2f57861b26c0f1a785aec2c6d195a81b124c421c
colemonnahan <monnahc@uw.edu>
2014-08-06 15:13:00 -0700

    updated code to install development branch of ss3sim

--- run_tests.R
+++ run_tests.R
@@ -1,10 +1,14 @@
+library(devtools)
 ## Update the development tools, if you haven't recently
 update.packages(c('knitr', 'devtools', 'roxygen2'))
 ## Build the ss3sim developement package
 remove.packages("ss3sim")
-devtools::install_github(repo="ss3sim", usernamer="ss3sim", ref="feature/change_bins")
+## install whatever branch we are working on
+install_github(repo="ss3sim", username="ss3sim",
+               ref="feature/change_bins")
 library(ss3sim)
 library(r4ss)
+change_bin
 
 dir.create("cod-om-test")
 files <- list.files("cod-om", full.names=TRUE)
