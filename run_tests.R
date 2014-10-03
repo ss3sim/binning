3dd20e3642f21d2b53439ccaa7e6107ef15a8f5f
colemonnahan <monnahc@uw.edu>
2014-10-03 14:21:24 -0700

    updated code for prelim tests of change_bin

--- run_tests.R
+++ run_tests.R
@@ -38,29 +38,27 @@ user.recdevs <- matrix(data=rnorm(100^2, mean=0, sd=.001),
 ### ------------------------------------------------------------
 
 
-
 ### ------------------------------------------------------------
 ### Preliminary bin analysis with cod
 ## WRite the cases to file
-## Run them in parallel
 bin.n <- 5
 bin.seq <- seq(2, 10, len=bin.n)
 for(i in 1:bin.n){
     bin <- seq(20, 150, by=bin.seq[i])
     x <- c(paste("bin_vector; seq(20, 150, by=", bin.seq[i],")"),
            "file_in; ss3.dat",
-           "file_out; ss3.dat", "type; \"length\"", "pop_bin;NULL", "write_file;1")
+           "file_out; ss3.dat", "type; \"len\"", "pop_bin;NULL", "write_file;1")
     writeLines(x, con=paste0(case_folder, "/B",i, "-cod.txt"))
 }
+get_args('cases/B1-cod.txt')
 scen.df <- data.frame(B.value=bin.seq, B=paste0("B", 1:bin.n))
 scen <- expand_scenarios(cases=list(D=0, E=0, F=0, R=0,M=0, B=1:bin.n), species="cod")
-run_ss3sim(iterations = 1, scenarios = scen, parallel=FALSE,
+run_ss3sim(iterations = 1:10, scenarios = scen, parallel=TRUE,
            case_folder = case_folder, om_dir = om,
            em_dir = em, case_files = list(M = "M", F = "F", D =
-    c("index", "lcomp", "agecomp"), R = "R", E = "E", B="B"),
-           user_recdevs=user.recdevs)
+    c("index", "lcomp", "agecomp"), R = "R", E = "E", B="B"))
 ## Read in the results and convert to relative error in long format
-get_results_all(user_scenarios=scen)
+get_results_all(user_scenarios=scen, over=TRUE)
 file.copy("ss3sim_scalar.csv", "results/bin_test1_scalar.csv", over=TRUE)
 file.copy("ss3sim_ts.csv", "results/bin_test1_ts.csv", over=TRUE)
 results <- read.csv("results/bin_test1_scalar.csv")
@@ -69,24 +67,25 @@ results_re <- as.data.frame(
     sapply(1:length(em_names), function(i)
            (results[,em_names[i]]- results[,gsub("_em", "_om", em_names[i])])/
            results[,gsub("_em", "_om", em_names[i])]
-           ))
+            ))
 names(results_re) <- gsub("_em", "_re", em_names)
 results_re$B <- results$B
+results_re$replicate <- results$replicate
 results_re <- results_re[sapply(results_re, function(x) any(is.finite(x)))]
 results_re <- results_re[sapply(results_re, function(x) !all(x==0))]
 results_re <- results_re[, names(results_re)[-grep("NLL",names(results_re))]]
-results_long <- reshape2::melt(results_re, "B")
+results_long <- reshape2::melt(results_re, c("B", "replicate"))
 results_long <- merge(scen.df, results_long)
 results_long$B <- factor(results_long$B, levels=paste0("B", 1:bin.n))
 ## Make exploratory plots
 ggplot(results_long, aes(x=B.value, y=value))+ ylab("relative error")+
-    geom_jitter()+facet_wrap("variable", scales="fixed") + ylim(-1,1) +
+    geom_line(aes(group=replicate))+facet_wrap("variable", scales="fixed") + ylim(-1,1) +
     xlab("bin width")
 ggsave("plots/bin_test1.png", width=10, height=7)
 ## Clean up everything
 unlink(scen, TRUE)
 file.remove(c("ss3sim_scalar.csv", "ss3sim_ts.csv"))
-rm(results, results_re, results_long, scen.df, scen, em_names, bin.seq, bin, x, i)
+rm(results, results_re, results_long, scen.df, scen, em_names, bin.seq, bin.n, x, i)
 ## End of tail compression run
 ### ------------------------------------------------------------
 
@@ -106,11 +105,10 @@ scen.df <- data.frame(T.value=c(-.001,tc.seq), T=paste0("T", 0:tc.n))
 scen <- expand_scenarios(cases=list(D=0, E=0, F=0, R=0,M=0, T=0:tc.n), species="cod")
 ## Run them in parallel
 ## RUns parallel=F but not TRUE??????
-run_ss3sim(iterations = 1:5, scenarios = scen, parallel=TRUE,
+run_ss3sim(iterations = 1:10, scenarios = scen, parallel=TRUE,
            case_folder = case_folder, om_dir = om,
            em_dir = em, case_files = list(M = "M", F = "F", D =
-    c("index", "lcomp", "agecomp"), R = "R", E = "E", T="T"),
-           user_recdevs=user.recdevs)
+    c("index", "lcomp", "agecomp"), R = "R", E = "E", T="T"))
 ## Read in the results and convert to relative error in long format
 get_results_all(user_scenarios=scen)
 file.copy("ss3sim_scalar.csv", "results/tc_test1_scalar.csv", over=TRUE)
@@ -123,16 +121,17 @@ results_re <- as.data.frame(
            results[,gsub("_em", "_om", em_names[i])]
            ))
 names(results_re) <- gsub("_em", "_re", em_names)
+results_re$replicate <- results$replicate
 results_re$T <- results$T
 results_re <- results_re[sapply(results_re, function(x) any(is.finite(x)))]
 results_re <- results_re[sapply(results_re, function(x) !all(x==0))]
 results_re <- results_re[, names(results_re)[-grep("NLL",names(results_re))]]
-results_long <- reshape2::melt(results_re, "T")
+results_long <- reshape2::melt(results_re, c("T", "replicate"))
 results_long <- merge(scen.df, results_long)
 results_long$T <- factor(results_long$T, levels=paste0("T", 0:tc.n))
 ## Make exploratory plots
 ggplot(results_long, aes(x=T.value, y=value))+ ylab("relative error")+
-    geom_jitter()+facet_wrap("variable", scales="fixed") + ylim(-1,1) +
+    geom_line(aes(group=replicate))+facet_wrap("variable", scales="fixed") + ylim(-1,1) +
     xlab("tail compression value")
 ggsave("plots/tc_test1.png", width=10, height=7)
 ## Clean up everything
@@ -152,14 +151,15 @@ for(i in 1:lc.n){
     x <- c(paste("lcomp_constant;", lc), "file_in; ss3.dat", "file_out; ss3.dat")
     writeLines(x, con=paste0(case_folder, "/C",i, "-cod.txt"))
 }
+## (xx <- get_args("cases/C1-cod.txt"))
+## add_nulls(xx, c("lcomp_constant", "file_in", "file_out"))
 scen.df <- data.frame(C.value=c(lc.seq), C=paste0("C", 1:lc.n))
 scen <- expand_scenarios(cases=list(D=0, E=0, F=0, R=0,M=0, C=1:lc.n), species="cod")
 ## Run them in parallel
-run_ss3sim(iterations = 1:5, scenarios = scen, parallel=TRUE,
+run_ss3sim(iterations = 1:10, scenarios = scen, parallel=TRUE,
            case_folder = case_folder, om_dir = om,
            em_dir = em, case_files = list(M = "M", F = "F", D =
-    c("index", "lcomp", "agecomp"), R = "R", E = "E", C="C"),
-           user_recdevs=user.recdevs)
+    c("index", "lcomp", "agecomp"), R = "R", E = "E", C="C"))
 ## Read in the results and convert to relative error in long format
 get_results_all(user_scenarios=scen)
 file.copy("ss3sim_scalar.csv", "results/lc_test1_scalar.csv", over=TRUE)
@@ -171,16 +171,17 @@ results_re <- as.data.frame(
            (results[,em_names[i]]- results[,gsub("_em", "_om", em_names[i])])/
            results[,gsub("_em", "_om", em_names[i])]))
 names(results_re) <- gsub("_em", "_re", em_names)
+results_re$replicate <- results$replicate
 results_re$C <- results$C
 results_re <- results_re[sapply(results_re, function(x) any(is.finite(x)))]
 results_re <- results_re[sapply(results_re, function(x) !all(x==0))]
 results_re <- results_re[,names(results_re)[-grep("NLL",names(results_re))]]
-results_long <- reshape2::melt(results_re, "C")
+results_long <- reshape2::melt(results_re, c("C", "replicate"))
 results_long <- merge(scen.df, results_long)
 results_long$C <- factor(results_long$C, levels=paste0("C", 0:lc.n))
 ## Make exploratory plots
 ggplot(results_long, aes(x=C.value, y=value))+ylab("relative error")+
-    geom_jitter()+facet_wrap("variable", scales="fixed") + ylim(-.25,.25) +
+    geom_line(aes(group=replicate))+facet_wrap("variable", scales="fixed") + ylim(-1,1) +
     xlab("robustification constant value")
 ggsave("plots/lc_test1.png", width=10, height=7)
 ## Clean up everything
@@ -191,27 +192,6 @@ rm(results, results_re, results_long, scen.df, scen, em_names, lc.seq, lc, x, i)
 ### ------------------------------------------------------------
 
 
-### ------------------------------------------------------------
-## Code for testing the change_bin function to run the base model
-setwd("F:/binning")
-dir.create("cod-om-test")
-files <- list.files("cod-om", full.names=TRUE)
-file.copy(files,  "cod-om-test")
-## to run the "change_bin" function
-dir.create("cod-om-test-new")
-files <- list.files("cod-om", full.names=TRUE)
-file.copy(files,  "cod-om-test-new")
-setwd("F:/binning/cod-om-test-new")
-change_bin(file_in="codOM.dat", file_out="codOM.new.dat", bin_vector = c(20, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59, 62, 65, 68, 71, 74, 77, 80, 83, 86, 89, 92, 95, 98, 101, 104, 107, 110, 113, 116, 119, 122, 125, 128, 131, 134, 137, 140, 143, 146, 149, 152), type="length", write_file = TRUE)
-## End of session so clean up
-unlink("cod-om-test", TRUE)
-unlink("cod-om-test-new", TRUE)
-## to check whether the new function works or not
-bin_or <- c(0.00260219, 0.0341899, 0.279936, 0.929283, 1.20611, 0.653391, 0.520717, 1.42683, 3.00334, 4.18986, 4.12005, 3.25679, 2.71086, 2.85671, 3.25729, 3.45327, 3.38539, 3.24925, 3.1975, 3.22675, 3.26419, 3.2722, 3.2615, 3.25157, 3.24677, 3.24085, 3.22745, 3.20273, 3.16287, 3.10237, 3.01459, 2.89321, 2.73361, 2.53419, 2.29732, 2.02987, 1.74285, 1.45006, 1.16618, 0.9046, 0.675517, 0.484856, 0.334057, 0.220715, 0.325783)
-bin_new <- c(0.0357928, 0.279938, 0.929293, 1.20612, 0.653397, 0.520722, 1.42684, 3.00337, 4.1899, 4.12009, 3.25683, 2.71089, 2.85673, 3.25732, 3.4533, 3.38542, 3.24928, 3.19754, 3.22678, 3.26422, 3.27223, 3.26153, 3.2516, 3.2468, 3.24088, 3.22748, 3.20276, 3.1629, 3.1024, 3.01462, 2.89323, 2.73364, 2.53421, 2.29734, 2.02989, 1.74287, 1.45007, 1.1662, 0.904609, 0.675524, 0.484861, 0.334061, 0.220717, 0.325786)
-plot(1:length(bin_new), bin_or[-1], type="p")
-points(1:length(bin_new), bin_new, col="red")
-### ------------------------------------------------------------
 
 
 ### ------------------------------------------------------------
