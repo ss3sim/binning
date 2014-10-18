f39f644d7d798a5c8cbca27fd44469eac1d46c50
colemonnahan <monnahc@uw.edu>
2014-10-17 17:57:48 -0700

    started developing wtatage sample function

--- test_sampling.R
+++ test_sampling.R
@@ -33,4 +33,33 @@ user.recdevs <- matrix(data=rnorm(100^2, mean=0, sd=.001),
 ### ------------------------------------------------------------
 
 
-## First try it with conditional age-at-length
+## First try it with conditional age-at-length. Make a copy of hte input
+## file to see how the change_bin function works on it.
+file.copy("cod-om/codOM.dat", "codOM.dat")
+dev_help("change_bins")
+
+## Took this manually from the length comps and age comps
+len_vector <- c(10, 53, 70, 83, 94, 101, 107, 111, 114, 117, 120, 123, 125,
+                127, 130, 132, 135, 138, 142, 148, 200)
+age_vector <- as.numeric(1:15)
+change_bin("codOM.dat", "codOM_cal.dat", bin_vector=age_vector,
+           type="cal")
+change_bin("codOM.dat", "codOM_age.dat", bin_vector=age_vector,
+           type="age")
+infile.age <- SS_readdat("codOM_age.dat", verbose=FALSE)
+infile.cal <- SS_readdat("codOM_cal.dat", verbose=FALSE)
+str(infile.age$agecomp)
+str(infile.cal$agecomp)
+
+## temp values for testing
+fleets <- c(1,2)
+years <- list(c(1998), c(1999,2001))
+Nsamp <- list(50, 15)
+cv <- list(.1,.3)
+## substr_r <- function(x, n){
+##   substr(x, nchar(x)-n+1, nchar(x))
+## }
+
+sample_calcomp(infile.cal, outfile="test.dat", fleets=NULL, years=years,
+               Nsamp=Nsamp, cv=cv)
+SS_readdat("test.dat")$agecomp
