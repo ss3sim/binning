5eff9007af7df58035f7e69ec140ac9b89e247e9
colemonnahan <monnahc@uw.edu>
2015-01-16 14:58:46 -0800

    another wave of sampling development, calcomp now seems to work

--- test_sampling.R
+++ test_sampling.R
@@ -9,15 +9,18 @@ source("startup.R")
 
 
 ## Create some simple, dummy sampling schemes, write them to test folder
-lcomp0 <- c('fleets;NULL', 'years;list(2)', 'Nsamp;list(45)', 'cpar;NULL')
-agecomp0 <- c('fleets;NULL', 'years;list(2)', 'Nsamp;list(55)', 'cpar;NULL')
-mlacomp0 <- c('fleets;NULL', 'Nsamp;65', 'years;list(2)')
-calcomp0 <- c('fleets;NULL', 'years;list(5)')
+lcomp0 <- c('fleets;NULL', 'years;list(2:3)', 'Nsamp;list(45:46)', 'cpar;NULL')
+agecomp0 <- c('fleets;c(1,2)', 'years;list(2:3, 78:79)',
+              'Nsamp;list(55:56, 4:5)', 'cpar;c(1,1)')
+mlacomp0 <- c('fleets;NULL', 'Nsamp;list(2:3)', 'years;list(2:3)')
+calcomp0 <- c('fleets;NULL', 'years;list(2:3)', 'Nsamp;list(45:46)')
+wtatage0 <- c('fleets;NULL','years;list(2:3)', 'cv_wtatage;.1')
 index1 <- c('fleets;2', 'years;list(25:99)', 'sds_obs;list(.1)')
-lcomp1 <- c('fleets;1', 'years;list(2)', 'Nsamp;list(45)', 'cpar;1')
-agecomp1 <- c('fleets;1', 'years;list(2)', 'Nsamp;list(55)', 'cpar;1')
-mlacomp1 <- c('fleets;1', 'Nsamp;65', 'years;list(2)')
-calcomp1 <- c('fleets;1', 'years;list(2)')
+lcomp1 <- c('fleets;1', 'years;list(2:3)', 'Nsamp;list(45:46)', 'cpar;c(1,1)')
+agecomp1 <- c('fleets;c(1,2)', 'years;list(2:3, 78:79)', 'Nsamp;list(55:56, 4:5)', 'cpar;c(1,1)')
+mlacomp1 <- c('fleets;1', 'Nsamp;list(2:3)', 'years;list(2:3)')
+calcomp1 <- c('fleets;1', 'years;list(2:3)', 'Nsamp;list(45:46)')
+wtatage1 <- c('fleets;c(1,2)','years;list(2:3, 78:79)', 'cv_wtatage;.1')
 writeLines(index1, paste0("test cases/index1-fla.txt"))
 writeLines(lcomp0, paste0("test cases/lcomp0-fla.txt"))
 writeLines(lcomp1, paste0("test cases/lcomp1-fla.txt"))
@@ -27,6 +30,8 @@ writeLines(mlacomp0, paste0("test cases/mlacomp0-fla.txt"))
 writeLines(mlacomp1, paste0("test cases/mlacomp1-fla.txt"))
 writeLines(calcomp0, paste0("test cases/calcomp0-fla.txt"))
 writeLines(calcomp1, paste0("test cases/calcomp1-fla.txt"))
+writeLines(wtatage0, paste0("test cases/wtatage0-fla.txt"))
+writeLines(wtatage1, paste0("test cases/wtatage1-fla.txt"))
 
 ### Now make
 ## All possible test cases:
@@ -35,28 +40,31 @@ writeLines(calcomp1, paste0("test cases/calcomp1-fla.txt"))
 # 3. Length + age comps
 # 4. Length + age comps + CAL
 # 5. Length + CAL
-# 6. Age + WatAge
-# 7. Age + MLA
+# 6. Age + MLA
+# 7. Age + WatAge
 
 ## ## not all combintions are supported
 ## scen <- expand_scenarios(cases=list(E=0, F=0, I=1, L=0:1, A=0:1, M=0:1,
 ##                          C=0:1),  species="fla")
-scen <- c("A0-C0-E0-F0-I1-L1-M0-fla",
-          "A1-C0-E0-F0-I1-L0-M0-fla",
-          "A1-C0-E0-F0-I1-L1-M0-fla",
-          "A1-C1-E0-F0-I1-L1-M0-fla",
-          "A0-C1-E0-F0-I1-L1-M0-fla",
-          "A0-C0-E0-F0-I1-L0-M1-fla")
+scen <- c("A0-C0-E0-W0-F0-I1-L1-M0-fla",
+          "A1-C0-E0-W0-F0-I1-L0-M0-fla",
+          "A1-C0-E0-W0-F0-I1-L1-M0-fla",
+          "A1-C1-E0-W0-F0-I1-L1-M0-fla",
+          "A0-C1-E0-W0-F0-I1-L1-M0-fla",
+          "A0-C0-E0-W0-F0-I1-L0-M1-fla",
+          "A1-C0-E0-W1-F0-I1-L0-M0-fla")
 case_files <- list(F = "F",  E="E",  I ="index", L="lcomp", A="agecomp",
-                   M="mlacomp", C="calcomp")
-a <- get_caseargs(folder = 'test cases', scenario = scen[1],
+                   M="mlacomp", C="calcomp", W="wtatage")
+a <- get_caseargs(folder = 'test cases', scenario = scen[7],
                   case_files = case_files)
-unlink(scen, TRUE)
+
+unlink(scen[7], TRUE)
 devtools::load_all("../ss3sim")
-run_ss3sim(iterations = 1, scenarios = scen[6], parallel=FALSE,
+run_ss3sim(iterations = 1, scenarios = scen[7], parallel=FALSE,
            case_folder = 'test cases', om_dir = fla_om,
            em_dir = fla_em, case_files=case_files)#, admb_options="-maxph 1")
 ## not working for Length and CAL only since age comps aren't there for sampling
+
 x
 
 ## ------------------------------------------------------------
