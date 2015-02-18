54a8db87a8560e92c055da9cad53a54d6490db72
colemonnahan <monnahc@uw.edu>
2015-02-18 15:22:10 -0800

    major clean up

--- write_casefiles.R
+++ write_casefiles.R
@@ -54,11 +54,21 @@ data1 <- c('age_bins; NULL', 'len_bins; seq(20, 160, by=2)',
            'pop_maximum_size; 202', 'lcomp_constant; 0.0001',
            'tail_compression; -1')
 writeLines(data1, con=paste0(case_folder,"/", "data1-cos.txt"))
-data2 <- c('age_bins; NULL', 'len_bins; seq(20, 160, by=20)',
+data2 <- c('age_bins; NULL', 'len_bins; seq(20, 160, by=5)',
            'pop_binwidth; 2', 'pop_minimum_size; 8',
            'pop_maximum_size; 202', 'lcomp_constant; 0.0001',
            'tail_compression; -1')
 writeLines(data2, con=paste0(case_folder,"/", "data2-cos.txt"))
+data3 <- c('age_bins; NULL', 'len_bins; seq(20, 160, by=10)',
+           'pop_binwidth; 2', 'pop_minimum_size; 8',
+           'pop_maximum_size; 202', 'lcomp_constant; 0.0001',
+           'tail_compression; -1')
+writeLines(data3, con=paste0(case_folder,"/", "data3-cos.txt"))
+data4 <- c('age_bins; NULL', 'len_bins; seq(20, 160, by=20)',
+           'pop_binwidth; 2', 'pop_minimum_size; 8',
+           'pop_maximum_size; 202', 'lcomp_constant; 0.0001',
+           'tail_compression; -1')
+writeLines(data4, con=paste0(case_folder,"/", "data4-cos.txt"))
 ## External binning cases. For this the change_data function shouldn't be
 ## called at all, and instead the generated data should be in 1cm bins
 ## using data0. em_binning0 is to ignore it and leave unchanged for use in internal cases.
@@ -66,7 +76,11 @@ em_binning0 <- c('lbin_method;NULL', 'bin_vector;seq(20,160, by=2)')
 writeLines(em_binning0, con=paste0(case_folder,"/", "em_binning0-cos.txt"))
 em_binning1 <- c('lbin_method;1', 'bin_vector;seq(20,160, by=2)')
 writeLines(em_binning1, con=paste0(case_folder,"/", "em_binning1-cos.txt"))
-em_binning2 <- c('lbin_method;1', 'bin_vector;seq(20,160, by=20)')
+em_binning2 <- c('lbin_method;1', 'bin_vector;seq(20,160, by=5)')
 writeLines(em_binning2, con=paste0(case_folder,"/", "em_binning2-cos.txt"))
+em_binning3 <- c('lbin_method;1', 'bin_vector;seq(20,160, by=10)')
+writeLines(em_binning3, con=paste0(case_folder,"/", "em_binning3-cos.txt"))
+em_binning4 <- c('lbin_method;1', 'bin_vector;seq(20,160, by=20)')
+writeLines(em_binning4, con=paste0(case_folder,"/", "em_binning4-cos.txt"))
 ## End of binning files
 ### ------------------------------------------------------------
