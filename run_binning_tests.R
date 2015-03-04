ab477eed7785ed6cc4690d6be06abb441d4e97c9
colemonnahan <monnahc@uw.edu>
2015-03-03 18:22:16 -0800

    added tests for ratio of observed

--- run_binning_tests.R
+++ run_binning_tests.R
@@ -5,25 +5,73 @@ source("startup.R")
 ## external case files:
 bin_width <- 10
 ## run function below first
-data10 <- run_binning_test(10)
-subset(data10, fleet==1 & year==71 & length==80)
-data5 <- run_binning_test(5)
-data13 <- run_binning_test(13)
-data2 <- run_binning_test(2)
-g10 <- ggplot(data10, aes(length, proportion, group=method, color=method))+
-    geom_line()+facet_wrap("year")
-g5 <- ggplot(data5, aes(length, proportion, group=method, color=method))+
-    geom_line()+facet_wrap("year")
-g13 <- ggplot(data13, aes(length, proportion, group=method, color=method))+
-    geom_line()+facet_wrap("year")
-g2 <- ggplot(data2, aes(length, proportion, group=method, color=method))+
-    geom_line()+facet_wrap("year")
-ggsave("plots/bin_tests_10cm.png", g10, width=12, height=7)
-ggsave("plots/bin_tests_13cm.png", g13, width=12, height=7)
-ggsave("plots/bin_tests_5cm.png", g5, width=12, height=7)
-ggsave("plots/bin_tests_2cm.png", g2, width=12, height=7)
 
-run_binning_test <- function(bin_width){
+plot.binning.test(6)
+
+internal.dir <- "B0-D1-E991-F1-I3-cod"
+external.dir <- "B3-D1-E991-F1-I0-cod"
+
+internal.reps <- list.dirs(internal.dir, recursive=FALSE, full=FALSE)
+external.reps <- list.dirs(external.dir, recursive=FALSE, full=FALSE)
+if(length(external.reps)!=length(internal.reps)) stop("not same reps")
+temp.list <- list()
+for(i in 1:length(internal.reps)){
+    int.temp <- SS_readdat(paste(internal.dir, internal.reps[i], "em", "ss3.dat", sep="/"), verb=FALSE)$lencomp
+    ext.temp <- SS_readdat(paste(external.dir, external.reps[i], "em", "ss3.dat", sep="/"), verb=FALSE)$lencomp
+    ratio.temp <- int.temp[,-(1:6)]/ext.temp[,-(1:6)]
+    temp.list[[i]]<- cbind(replicate=i, int.temp[,1:6], ratio.temp)
+}
+results <- do.call(rbind, temp.list)
+results.long <- melt(subset(df, FltSvy==fleet, select=-c(Seas, Nsamp,
+                                Gender, Part)), id.vars=c("Yr", "FltSvy", 'replicate'))
+results.long$variable <- as.numeric(gsub("l", "", results.long$variable))
+names(results.long) <- c('year', 'fleet', 'replicate', "length", "proportion")
+results.long$proportion[!is.finite(results.long$proportion)] <- NA
+results.long <- ddply(results.long, .(replicate, length), mutate,
+                 mean.proportion=mean(proportion, na.rm=TRUE))
+g <- ggplot(results.long, aes(length, mean.proportion))+
+    geom_line()+ facet_wrap("replicate") + geom_hline(yintercept=1, col=gray(.5))+
+    ggtitle("Ratio of observed internal/external; mean across years")
+ggsave(paste0("plots/ratio_tests_", id, ".png"),g)
+!!!!!left off here need to make above into a function and loop through different
+bin scenarios
+
+plot.binning.test <- function(bin_width, fleet=1){
+    df <- run.binning.test.ev(10)
+    g <- ggplot(df, aes(length, proportion, group=method, color=method))+
+        geom_line()+facet_wrap("year")
+    ggsave(paste0("plots/bin_tests_", bin_width, "cm.png"), g, width=12, height=7)
+}
+
+extract_expected_data2 <- function(data_ss_new = "data.ss_new") {
+    ## modified slightly from one in package for temporary external use
+    data_file <- readLines(data_ss_new)
+    x1 <- grep("#_expected values with no error added", data_file, fixed=TRUE)+1
+    x2 <- grep("ENDDATA", data_file)-2
+    if(length(x1)!=1 | length(x2)!=1)
+        stop("grep failed to find lines for extracting data, not of length 1")
+    if(x2<x1)
+        stop("Something wrong with OM data.ss_new file, last line comes before first line")
+    data_file_new <- data_file[x1:x2]
+    if(999 != as.numeric(data_file_new[length(data_file_new)]))
+        warning("last line of extracted expected values was not 999, was it read in correctly?")
+    writeLines(data_file_new, "temp.dat")
+    dat <- r4ss::SS_readdat("temp.dat", verb=FALSE)
+    file.remove("temp.dat")
+    return(dat)
+}
+
+melt.length <- function(df, method, fleet){
+    df.long <- melt(subset(df, FltSvy==fleet, select=-c(Seas, Nsamp, Gender, Part)), id.vars=c("Yr", "FltSvy"))
+    df.long$variable <- as.numeric(gsub("l", "", df.long$variable))
+    names(df.long) <- c('year', 'fleet', "length", "proportion")
+    df.long <- ddply(df.long, .(year, fleet), mutate,
+                     proportion=proportion/sum(proportion))
+    df.long$method <- method
+    return(df.long)
+}
+
+run.binning.test.ev <- function(bin_width, fleet=1){
     case_folder='cases'; species='cod'
     em_binning100 <- c('lbin_method;1', paste0('bin_vector;seq(20,160, by=', bin_width,')'))
     writeLines(em_binning100, con=paste0(case_folder,"/", "em_binning100-cod.txt"))
@@ -45,27 +93,11 @@ run_binning_test <- function(bin_width){
     scenarios <- expand_scenarios(cases=list(D=1, F=1, B=100:101, E=991), species=species)
     ## Dont care about estimated values so turn off estimation in the EM to
     ## speed up
+    unlink(scenarios, TRUE)
     run_ss3sim(iterations=1, scenarios=scenarios,
                case_folder=case_folder, om_dir=ss3model(species, "om"),
                em_dir=ss3model(species, "em"), case_files=case_files,
                call_change_data=TRUE, admb_options=" -maxfn 1")
-    extract_expected_data2 <- function(data_ss_new = "data.ss_new") {
-        ## modified slightly from one in package for temporary external use
-        data_file <- readLines(data_ss_new)
-        x1 <- grep("#_expected values with no error added", data_file, fixed=TRUE)+1
-        x2 <- grep("ENDDATA", data_file)-2
-        if(length(x1)!=1 | length(x2)!=1)
-            stop("grep failed to find lines for extracting data, not of length 1")
-        if(x2<x1)
-            stop("Something wrong with OM data.ss_new file, last line comes before first line")
-        data_file_new <- data_file[x1:x2]
-        if(999 != as.numeric(data_file_new[length(data_file_new)]))
-            warning("last line of extracted expected values was not 999, was it read in correctly?")
-        writeLines(data_file_new, "temp.dat")
-        dat <- r4ss::SS_readdat("temp.dat", verb=FALSE)
-        file.remove("temp.dat")
-        return(dat)
-    }
     ## Get the different length expected values; note need to do the external
     ## binning manually on the ss_new file to get expected values directly and
     ## avoid sampling uncertainty
@@ -73,37 +105,25 @@ run_binning_test <- function(bin_width){
     ## The external case is used to get the "truth" which is the original 1cm
     ## bins as well as the externally binned
     external.dat <- extract_expected_data2("B100-D1-E991-F1-cod/1/om/data.ss_new")
-    len.external <- change_em_binning(datfile=external.dat, file_out="xx", write_file=FALSE,
-                                      bin_vector=seq(20,160, by=bin_width), lbin_method=1)$lencomp
-    len.truth <- external.dat$lencomp
+    external <- change_em_binning(datfile=external.dat, file_out="xx", write_file=FALSE,
+                                  bin_vector=seq(20,160, by=bin_width), lbin_method=1)$lencomp
+    truth <- external.dat$lencomp
+    external.obs <- SS_readdat("B100-D1-E991-F1-cod/1/em/ss3.dat", verb=F)$lencomp
     ## The internal just has the one case
-    internal.dat <- extract_expected_data2("B101-D1-E991-F1-cod/1/om/data.ss_new")
-    len.internal <- internal.dat$lencomp
-    unlink(scenarios, TRUE)
-    c("data100-cod.txt", "data101-cod.txt", "em_binning100-cod.txt", "em_binning101-cod.txt")
-    ## Melt these down for plotting
+    internal <- extract_expected_data2("B101-D1-E991-F1-cod/1/om/data.ss_new")$lencomp
+    internal.obs <- SS_readdat("B101-D1-E991-F1-cod/1/em/ss3.dat", verb=F)$lencomp
     ## Get ratio of # bins to help scale the proportions later
-    num.bins.ratio <- ncol(len.external[-(1:6),])/ncol(len.truth[-(1:6),])
-    len.external.long <- melt(subset(len.external, FltSvy==1, select=-c(Seas, Nsamp, Gender, Part)), id.vars=c("Yr", "FltSvy"))
-    len.external.long$variable <- as.numeric(gsub("l", "", len.external.long$variable))
-    names(len.external.long) <- c('year', 'fleet', "length", "proportion")
-    len.external.long <- ddply(len.external.long, .(year, fleet), mutate,
-                               proportion=proportion/sum(proportion))
-    len.external.long$method <- "external"
-    len.truth.long <- melt(subset(len.truth, FltSvy==1, select=-c(Seas, Nsamp, Gender, Part)), id.vars=c("Yr", "FltSvy"))
-    len.truth.long$variable <- as.numeric(gsub("l", "", len.truth.long$variable))
-    names(len.truth.long) <- c('year', 'fleet', "length", "proportion")
-    len.truth.long <- ddply(len.truth.long, .(year, fleet), mutate,
-                            proportion=proportion/sum(proportion))
-    len.truth.long$proportion <-     len.truth.long$proportion/num.bins.ratio
-    len.truth.long$method <- "truth"
-    len.internal.long <- melt(subset(len.internal, FltSvy==1, select=-c(Seas, Nsamp, Gender, Part)), id.vars=c("Yr", "FltSvy"))
-    len.internal.long$variable <- as.numeric(gsub("l", "", len.internal.long$variable))
-    names(len.internal.long) <- c('year', 'fleet', "length", "proportion")
-    len.internal.long <- ddply(len.internal.long, .(year, fleet), mutate,
-                               proportion=proportion/sum(proportion))
-    len.internal.long$method <- "internal"
-
-    data <- rbind(len.external.long, len.truth.long, len.internal.long)
+    num.bins.ratio <- ncol(external[-(1:6),])/ncol(truth[-(1:6),])
+    ## Prep them for plotting
+    external <- melt.length(external, "external", fleet=fleet)
+    internal <- melt.length(internal, "internal", fleet=fleet)
+    external.obs <- melt.length(external.obs, "external.obs", fleet=fleet)
+    internal.obs <- melt.length(internal.obs, "internal.obs", fleet=fleet)
+    truth <- melt.length(truth, "truth", fleet=fleet)
+    truth$proportion <- truth$proportion/num.bins.ratio
+    data <- rbind(external, truth, internal)
+    ## clean up
+    unlink(scenarios, TRUE)
+    file.remove(paste0(case_folder, "/", c("data100-cod.txt", "data101-cod.txt", "em_binning100-cod.txt", "em_binning101-cod.txt")))
     return(invisible(data))
 }
