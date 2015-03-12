d5b3b07f9eb9e179212188becbf240aa055f77ae
mrudd <merrillrudd@gmail.com>
2015-03-12 15:57:51 -0700

    cod cases updated

--- cases/write_cases_binning.R
+++ cases/write_cases_binning.R
@@ -90,28 +90,28 @@ write_bincase(species="cod", binwidth=24, lbmin=20, lbmax=160,
 ## External binning cases. For this the change_data function shouldn't be
 ## called at all, and instead the generated data should be in 1cm bins
 ## using data0. em_binning0 is to ignore it and leave unchanged for use in internal cases.
-em_binning0 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=1)')
-writeLines(em_binning0, con=paste0(case_folder,"/","em_binning0-cod.txt"))
-em_binning1 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=2)')
-writeLines(em_binning1, con=paste0(case_folder,"/","em_binning1-cod.txt"))
-em_binning2 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=4)')
-writeLines(em_binning2, con=paste0(case_folder,"/","em_binning2-cod.txt"))
-em_binning3 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=13)')
-writeLines(em_binning3, con=paste0(case_folder,"/","em_binning3-cod.txt"))
+# em_binning0 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=1)')
+# writeLines(em_binning0, con=paste0(case_folder,"/","em_binning0-cod.txt"))
+# em_binning1 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=2)')
+# writeLines(em_binning1, con=paste0(case_folder,"/","em_binning1-cod.txt"))
+# em_binning2 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=4)')
+# writeLines(em_binning2, con=paste0(case_folder,"/","em_binning2-cod.txt"))
+# em_binning3 <- c('lbin_method;2', 'pop_binwidth;1', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=13)')
+# writeLines(em_binning3, con=paste0(case_folder,"/","em_binning3-cod.txt"))
 
-em_binning11 <- c('lbin_method;2', 'pop_binwidth;2', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=2)')
-writeLines(em_binning11, con=paste0(case_folder,"/","em_binning11-cod.txt"))
-em_binning12 <- c('lbin_method;2', 'pop_binwidth;4', 'pop_minimum_size;8',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=4)')
-writeLines(em_binning12, con=paste0(case_folder,"/","em_binning12-cod.txt"))
-em_binning13 <- c('lbin_method;2', 'pop_binwidth;13', 'pop_minimum_size;7',
-                 'pop_maximum_size;202', 'bin_vector;seq(20,160,by=13)')
-writeLines(em_binning13, con=paste0(case_folder,"/","em_binning13-cod.txt"))
+# em_binning11 <- c('lbin_method;2', 'pop_binwidth;2', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=2)')
+# writeLines(em_binning11, con=paste0(case_folder,"/","em_binning11-cod.txt"))
+# em_binning12 <- c('lbin_method;2', 'pop_binwidth;4', 'pop_minimum_size;8',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=4)')
+# writeLines(em_binning12, con=paste0(case_folder,"/","em_binning12-cod.txt"))
+# em_binning13 <- c('lbin_method;2', 'pop_binwidth;13', 'pop_minimum_size;7',
+#                  'pop_maximum_size;202', 'bin_vector;seq(20,160,by=13)')
+# writeLines(em_binning13, con=paste0(case_folder,"/","em_binning13-cod.txt"))
 
 
 
