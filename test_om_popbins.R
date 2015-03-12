14fe6a3639f2a52d02d15feb3e47918a569a6170
Sean Anderson <sean@seananderson.ca>
2015-03-11 22:54:33 -0700

    Setup om popbin test apply function

--- test_om_popbins.R
+++ test_om_popbins.R
@@ -1,29 +1,30 @@
 # Test the effect of changing the population bin size on the OM
 
-library("ss3models")
-library("ss3sim")
-library("r4ss")
-library("dplyr")
+out <- lapply(seq(1, 20), function(x) {
+  file.copy(ss3models::ss3model("cod", "om"), to = ".", recursive = TRUE)
+  file.rename("om", "cod")
+  datfile <- SS_readdat("cod/ss3.dat", verbose = FALSE)
+  ss3sim::change_data(
+    datfile          = datfile,
+    outfile          = "cod/ss3.dat",
+    fleets           = 1:2,
+    years            = 26:100,
+    types            = c("len", "age"),
+    age_bins         = 1:15,
+    len_bins         = seq(20, 155, x),
+    pop_binwidth     = x,
+    pop_minimum_size = NULL,
+    pop_maximum_size = NULL)
 
-file.copy(ss3model("cod", "om"), to = ".", recursive = TRUE)
-file.rename("om", "cod")
-datfile <- SS_readdat("cod/ss3.dat", verbose = FALSE)
-change_data(
-  datfile          = datfile,
-  outfile          = "cod/ss3.dat",
-  fleets           = 1:2,
-  years            = 26:100,
-  types            = c("len", "age"),
-  age_bins         = 1:15,
-  len_bins         = seq(20, 155, 30),
-  pop_binwidth     = 30,
-  pop_minimum_size = NULL,
-  pop_maximum_size = NULL)
+  system("cd cod; ss3_24o_safe -noest")
+  o <- r4ss::SS_output("cod", covar = FALSE, verbose = FALSE, compfile = "none",
+    forecast = TRUE, warn = TRUE, readwt = FALSE, printstats = FALSE,
+    NoCompOK = TRUE, ncols = 300)
+  unlink("cod", recursive = TRUE, force = TRUE)
+  Sys.sleep(1) # give ample time for deletion of folder
+  dq <- o$derived_quants
+  dq
+})
 
-system("cd cod; ss3_24o_safe -noest")
-o <- SS_output("cod", covar = FALSE, verbose = FALSE, compfile = "none",
-  forecast = TRUE, warn = TRUE, readwt = FALSE, printstats = FALSE,
-  NoCompOK = TRUE, ncols = 300)
-unlink("cod", recursive = TRUE, force = TRUE)
-dq <- o$derived_quants
-#filter(dq, LABEL == "SSB_MSY") %>% select(Value)
+# library("dplyr")
+# filter(dq, LABEL == "SSB_MSY") %>% select(Value)
