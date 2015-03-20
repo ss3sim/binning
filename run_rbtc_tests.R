8d1c6af30f726e1d9aeb08c4e90e63a851a8b8b7
Kotkot <kotarono@uw.edu>
2015-03-20 14:55:06 -0700

    updated

--- run_rbtc_tests.R
+++ run_rbtc_tests.R
@@ -1,14 +1,16 @@
 ## To test the tail compression
-scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=11:14, B=c(1,2,3), E=991), species=species)
 case_files <- list(F="F", B="em_binning", I="data",
                    D=c("index","lcomp","agecomp","calcomp"), E="E")
 ## unlink(scenarios, TRUE)
 # scenarios <- scenarios[! scenarios %in% list.dirs(getwd(),recursive=F, full.names =F)]
 Nsim <- 25
-run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-           parallel=TRUE, parallel_iterations=FALSE,
-           case_folder=case_folder, om_dir=ss3model(species, "om"),
-           em_dir=ss3model(species, "em"), case_files=case_files, call_change_data=TRUE)
+for(spp in species){
+	scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=11:14, B=c(1,2,3), E=991), species=spp)
+	run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
+						 parallel=TRUE, parallel_iterations=FALSE,
+						 case_folder=case_folder, om_dir=ss3model(spp, "om"),
+						 em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
+}
 ## Read in results
 get_results_all(user=scenarios, parallel=TRUE, over=TRUE)
 xx <- read.csv("ss3sim_scalar.csv")
@@ -18,16 +20,18 @@ saveRDS(xx, file="results/results_tcomp.ts.RData")
 file.remove(c('ss3sim_ts.csv', 'ss3sim_scalar.csv'))
 
 ## To test the robustification constant
-scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=21:24, B=c(1,2,3), E=991), species=species)
 case_files <- list(F="F", B="em_binning", I="data",
                    D=c("index","lcomp","agecomp","calcomp"), E="E")
 ## unlink(scenarios, TRUE)
 # scenarios <- scenarios[! scenarios %in% list.dirs(getwd(),recursive=F, full.names =F)]
 Nsim <- 25
-run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-           parallel=TRUE, parallel_iterations=FALSE,
-           case_folder=case_folder, om_dir=ss3model(species, "om"),
-           em_dir=ss3model(species, "em"), case_files=case_files, call_change_data=TRUE)
+for(spp in species){
+	scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=21:24, B=c(1,2,3), E=991), species=spp)
+	run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
+						 parallel=TRUE, parallel_iterations=FALSE,
+						 case_folder=case_folder, om_dir=ss3model(spp, "om"),
+						 em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
+}
 ## Read in results
 get_results_all(user=scenarios, parallel=TRUE, over=TRUE)
 xx <- read.csv("ss3sim_scalar.csv")
