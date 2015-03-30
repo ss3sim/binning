0d573ed71309557bcf86d82df7d6bc0ecb04d784
colemonnahan <monnahc@uw.edu>
2015-03-30 10:18:21 -0700

    added output csv files to gitignore and instead of deleting them

--- run_rbtc_tests.R
+++ run_rbtc_tests.R
@@ -5,11 +5,11 @@ case_files <- list(F="F", B="em_binning", I="data",
 # scenarios <- scenarios[! scenarios %in% list.dirs(getwd(),recursive=F, full.names =F)]
 Nsim <- 25
 for(spp in species){
-	scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=11:14, B=c(1,2,3), E=991), species=spp)
-	run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-						 parallel=TRUE, parallel_iterations=FALSE,
-						 case_folder=case_folder, om_dir=ss3model(spp, "om"),
-						 em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
+    scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=11:14, B=c(1,2,3), E=991), species=spp)
+    run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
+               parallel=TRUE, parallel_iterations=FALSE,
+               case_folder=case_folder, om_dir=ss3model(spp, "om"),
+               em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
 }
 ## Read in results
 scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=11:14, B=c(1,2,3), E=991), species=species)
@@ -18,7 +18,7 @@ xx <- read.csv("ss3sim_scalar.csv")
 saveRDS(xx, file="results/results_tcomp.sc.RData")
 xx <- read.csv("ss3sim_ts.csv")
 saveRDS(xx, file="results/results_tcomp.ts.RData")
-file.remove(c('ss3sim_ts.csv', 'ss3sim_scalar.csv'))
+## file.remove(c('ss3sim_ts.csv', 'ss3sim_scalar.csv'))
 
 ## To test the robustification constant
 case_files <- list(F="F", B="em_binning", I="data",
@@ -27,11 +27,11 @@ case_files <- list(F="F", B="em_binning", I="data",
 # scenarios <- scenarios[! scenarios %in% list.dirs(getwd(),recursive=F, full.names =F)]
 Nsim <- 25
 for(spp in species){
-	scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=21:24, B=c(1,2,3), E=991), species=spp)
-	run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-						 parallel=TRUE, parallel_iterations=FALSE,
-						 case_folder=case_folder, om_dir=ss3model(spp, "om"),
-						 em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
+    scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=21:24, B=c(1,2,3), E=991), species=spp)
+    run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
+               parallel=TRUE, parallel_iterations=FALSE,
+               case_folder=case_folder, om_dir=ss3model(spp, "om"),
+               em_dir=ss3model(spp, "em"), case_files=case_files, call_change_data=TRUE)
 }
 ## Read in results
 scenarios <- expand_scenarios(cases=list(D=c(2,3,5,6), F=1, I=21:24, B=c(1,2,3), E=991), species=species)
@@ -40,7 +40,7 @@ xx <- read.csv("ss3sim_scalar.csv")
 saveRDS(xx, file="results/results_robust.sc.RData")
 xx <- read.csv("ss3sim_ts.csv")
 saveRDS(xx, file="results/results_robust.ts.RData")
-file.remove(c('ss3sim_ts.csv', 'ss3sim_scalar.csv'))
+## file.remove(c('ss3sim_ts.csv', 'ss3sim_scalar.csv'))
 
 ## clean up work space
 rm(xx, scenarios, Nsim, case_files)
