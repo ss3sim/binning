3b379e93348f3d96db4f9f8be31ee8b84ee23ee4
colemonnahan <monnahc@uw.edu>
2015-04-02 18:39:43 -0700

    started working on figures, added second

--- make_figures.R
+++ make_figures.R
@@ -6,142 +6,213 @@ library(dplyr)
 ## global options
 width <- 7                             # inches
 height <- 5
+col.label <- gray(.2)
+col.border <- gray(.5)
+col.tick <- gray(.5)
 
+## 1. image plot / figure/table of lit. review
+## 2. experimental design
+## 3. OM bin stuff - flatfish - send code for spawn-bio-RE to Cole
+## 4. estimation bin results - add beanaplots to function, put it in makefigure file flatfish
+## 5. run time and accuracy
+## 6. robustifation
+## 7. tail compression
 
-### ------------------------------------------------------------
-## Make the literature review figure(s)
-lit <- read.csv("lit-review.csv", stringsAsFactors = FALSE, strip.white = TRUE)
-names(lit) <- tolower(names(lit))
-names(lit) <- gsub("\\.", "_", names(lit))
-names(lit) <- gsub("__", "_", names(lit))
-names(lit) <- gsub("_$", "", names(lit))
-
-region_lit <- lit %>% group_by(region, bin_width_cm) %>%
-  summarise(num = length(bin_width_cm))
-taxa_lit <- lit %>% group_by(species_type, bin_width_cm) %>%
-  summarise(num = length(bin_width_cm))
-
-region_lit_wide <- reshape2::dcast(region_lit, region ~ bin_width_cm,
-  value.var = "num")
-region_lit_wide[is.na(region_lit_wide)] <- 0
-
-taxa_lit_wide <- reshape2::dcast(taxa_lit, species_type ~ bin_width_cm,
-  value.var = "num")
-taxa_lit_wide[is.na(taxa_lit_wide)] <- 0
-
-region_order <- c("Australia", "Atlantic", "Caribbean", "Gulf of Mexico",
-  "South Atlantic", "West Coast")
-region_lit_wide <- region_lit_wide[order(region_order, region_lit_wide$region), ]
-region_lit_wide$region[-1] <- paste("US", region_lit_wide$region[-1])
-
-fable <- function(x, y, z, xlab = "", ylab = "",
-  col_breaks = seq(0.5, 4, length.out = 13), log_colours = TRUE,
-  text_cex = 1) {
-
-  z <- z[nrow(z):1, ]
-  y <- rev(y)
-  col <- rev(grey(seq(0, 1, length.out = 18)))
-  col <- col[2:13]
-  if (log_colours)
-    z_plot <- log(z + 1)
-  else
-    z_plot <- z
-  image(1:length(x), 1:length(y), t(z_plot), axes = F, xlab = "", ylab = "",
-    col = col, breaks = col_breaks)
-  x_lines <- 1:(length(x) + 1) - 0.5
-  if(!is.null(y))
-    y_lines <- 1:(length(y) + 1) - 0.5
-  axis(3, at = x_lines, labels = FALSE, col = "grey50")
-  axis(3, tick = FALSE, at = 1:length(x), labels = x, col = "grey50", padj = 0.4)
-  axis(2, at = y_lines, labels = FALSE, col = "grey50")
-  axis(2, tick = FALSE, at = 1:length(y), labels = y, col = "grey50",
-    padj = 0.5, hadj = 0.9, las = 1)
-  abline(h = y_lines, col = "#00000050")
-  abline(v = x_lines, col = "#00000050")
-  for(x_i in 1:length(x)) {
-    for(y_i in 1:length(y)) {
-      col <- ifelse(z[y_i, x_i] == 0, "white", "grey10")
-      text(x_i, y_i, z[y_i, x_i], cex = text_cex, col = col)
-    }
-  }
-  box(col = "grey50")
-  mtext(xlab, side = 3, line = 1.75, cex = 0.8)
-  if(!is.null(y))
-    mtext(ylab, side = 2, line = 2.00, cex = 0.8)
+print.letter <- function(label="(a)",xy=c(0.1,0.925),...) {
+    tmp <- par("usr")
+    text.x <- tmp[1]+xy[1]*diff(tmp[1:2])   #x position, diff=difference
+    text.y <- tmp[3]+xy[2]*diff(tmp[3:4])   #y position
+    text(x=text.x, y=text.y, labels=label, ...)
+}
+make.file <- function(type=c("png","pdf", "none"), filename,
+                      width, height, res){
+    ## Pass it file type and dimensions in inches. It creates file.
+    type <- match.arg(type)
+    ## If no extension given, add one
+    if(length(grep(type, filename))==0)
+        filename <- paste0(filename,".",type)
+    if(type=="png") png(filename, width=width,
+       height=height, units="in", res=res)
+    else if(type=="pdf"){pdf(filename, width=width, height=height)}
+    else if(dev.cur()==1) dev.new(width=width, height=height)
 }
 
-pdf("plots/lit-review.pdf", width = 6, height = 7)
-par(mar = c(1, 8, 3, 1), oma = c(1, 1, 1, 1), cex = 0.75)
-m <- c(rep(1, 3), rep(2, 5))
-layout(m)
-z <- region_lit_wide[,-1] %>% as.matrix
-fable(y = region_lit_wide$region, x = names(region_lit_wide)[-1], z = z, text_cex = 0.9)
-z <- taxa_lit_wide[,-1] %>% as.matrix
-fable(y = taxa_lit_wide$species_type, x = names(taxa_lit_wide)[-1], z = z, text_cex = 0.9)
+
+
+
+## stripped this out of SSplotSelex -- just grabbed the pieces we need
+
+col1 <- 'black'
+extract.data <- function(replist){
+    attach(replist)
+        growdat <- replist$endgrowth
+        growdatF <- growdat[growdat$Gender == 1 & growdat$Morph == mainmorphs[1], ]
+        growdatF$Sd_Size <- growdatF$SD_Mid
+        growdatF$high <- qnorm(0.975, mean = growdatF$Len_Mid, sd = growdatF$Sd_Size)
+        growdatF$low <- qnorm(0.025, mean = growdatF$Len_Mid, sd = growdatF$Sd_Size)
+        plotlenselex <- as.numeric(sizeselex[sizeselex$Factor ==
+                                             "Lsel" & sizeselex$year == endyr & sizeselex$Fleet ==
+                                             1 & sizeselex$gender == 1, -(1:5)])
+        plotageselex <-
+            as.numeric(ageselex[ageselex$factor ==
+                                "Asel" & ageselex$year == endyr & ageselex$fleet ==
+                                1 & ageselex$gender == 1, -(1:7)])
+        x <- seq(0, accuage, by = 1)
+        y <- lbinspop
+        z <- plotageselex %o% plotlenselex
+        xx <- list(x=x, y=y, z=z, mid=growdatF$Len_Mid, low=growdatF$low,
+                   high=growdatF$high)
+    detach(replist)
+    return(xx)
+}
+f <- function(x, low, mid, high){
+    lines(x, mid, col = "white", lwd = 5)
+    lines(x, mid, col = col1, lwd = 3)
+    lines(x, high, col = "white", lwd = 1, lty = 1)
+    lines(x, high, col = col1, lwd = 1, lty = "dashed")
+    lines(x, low, col = "white", lwd = 1, lty = 1)
+    lines(x, low, col = col1, lwd = 1, lty = "dashed")
+}
+flatfish <- extract.data(r4ss::SS_output(file.path("B0-D2-F1-I0-flatfish", "1", "om/"), covar=F, forecast=F,
+                           ncols=300, readwt=FALSE, printstats=FALSE, verbose=FALSE))
+cod <- extract.data(r4ss::SS_output(file.path("B0-D2-F1-I0-cod", "1", "om/"), covar=F, forecast=F,
+                           ncols=300, readwt=FALSE, printstats=FALSE, verbose=FALSE))
+yellow <- extract.data(r4ss::SS_output(file.path("xB0-D2-F1-I0-yellow/", "1", "om/"), covar=F, forecast=F,
+                           ncols=300, readwt=FALSE, printstats=FALSE, verbose=FALSE))
+replist.list <- list(flatfish,cod,yellow)
+
+## flatfish.dat <- SS_readdat(file.path("example_models", scenario, "1", "em/ss3.dat"), verbose=FALSE)
+## lbins <- dat$lbin_vector
+## agebins <- dat$agebin_vector
+## ## Get length samples aggegrated across year for specified fleet
+## lbin.proportions <- as.vector(colSums(dat$lencomp[dat$lencomp[,3]==fleet,-(1:6)]))
+## lbin.proportions <- lbin.proportions/max(lbin.proportions)*circle.scalar
+## agebin.proportions <- as.vector(colSums(dat$agecomp[dat$agecomp[,3]==fleet,-(1:9)]))
+## agebin.proportions <- agebin.proportions/max(agebin.proportions)*circle.scalar
+
+
+## on.exit(if(type!="none") dev.off())
+## make.file(type=type, paste("Figures/Figure4", res,
+##           sep="_"), width=width, height=height, res=res)
+
+make.file("png", filename="figures/figure2_expdesign.png", width=width,
+          height=6, res=500)
+set.seed(5)
+par(mar=c(2,1,1,1), tck=-0.015, oma=c(1,2,0,0),
+    mgp=c(.5, .075,0), mfcol=c(1,1), cex.lab=.9, cex.axis=.8,
+    col.axis=col.label, xpd=TRUE)
+bin.widths <- c(1,5,10,20)
+n.contours <- 8
+cols.palette <- RColorBrewer::brewer.pal(n.contours, "Blues")
+layout(mat=matrix(c(1,1,2,2,3,3,4,4,4,5,5,5), nrow=2, byrow=TRUE), heights=c(1,.4))
+for(i in 1:3){
+with(replist.list[[i]],{
+    plot(0,0, ylim=c(0, max(y)), xlim=c(-5, max(x)+5), type='n',
+         axes=FALSE, ann=FALSE)
+    contour(x, y, z, nlevels=n.contours , col=cols.palette, lwd=1,
+            axes=FALSE, add=TRUE)
+    box(col=col.border)
+    f(x=x, low,mid,high)
+    temp.ages <- rnorm(1000, 10,5)
+    den <- density(temp.ages, from=0, to=max(x))
+    lines(den$x, den$y*100)
+    temp.len <- rnorm(1000, 20, 5)
+    den <- density(temp.len, from=0, to=max(y))
+    lines(y=seq(min(den$x), max(den$x), len=length(den$x)), x=max(x)-den$y*100)
+        sapply(seq_along(bin.widths), function(ii) {
+            ynew <- seq(y[1], y[length(y)], by=bin.widths[ii])
+            points(x=rep(-ii-1, len=length(ynew)), y=ynew, pch="-")
+        })
+    axis(1, col=col.border, mgp=par()$mgp, tck=par()$tck)
+    axis(2, col=col.border, mgp=par()$mgp, tck=par()$tck)
+    mtext("Age", side=1, line=1, cex=par()$cex.lab)
+    if(i==1) mtext("Length (cm)", side=2, line=1,cex=par()$cex.lab)
+    mtext(c("Flatfish", "Cod", "Rockfish")[i], side=3, line=-1.5, cex=par()$cex.lab*1.1)
+})}
+cex.fishery <- 1.5
+cex.survey <- 1
+## add data rich
+plot(x=25:100, ylim=c(1,4.5), axes=FALSE, ann=FALSE, xlim=c(25,100))
+lcomp2 <- ss3sim:::get_args("cases/lcomp2-cod.txt")
+with(lcomp2, points(years[[1]], y=rep(2, len=length(years[[1]])), cex=cex.fishery))
+with(lcomp2, points(years[[2]], y=rep(2.25, len=length(years[[2]])), cex=cex.survey, pch=16))
+index2 <- ss3sim:::get_args("cases/index2-cod.txt")
+with(index2, points(years[[1]], y=rep(3, len=length(years[[1]])), cex=cex.fishery))
+agecomp2 <- ss3sim:::get_args("cases/agecomp2-cod.txt")
+with(agecomp2, points(years[[1]], y=rep(1, len=length(years[[1]])), cex=cex.fishery))
+with(agecomp2, points(years[[2]], y=rep(1.25, len=length(years[[2]])), cex=cex.survey, pch=16))
+points(x=25:100, y=rep(4, len=76), cex=cex.fishery)
+axis(1, at=seq(25,100, by=25), col=col.border, mgp=par()$mgp, tck=par()$tck)
+text(x=90, y=c(1.5,2.5,3.25,4.25), labels=c("Lengths", "Ages", "Index", "Catches"))
+box(col=col.border)
+## add data poor
+mtext("Year", side=1, line=1,cex=par()$cex.lab)
+mtext("Data Type", side=2, line=.75 ,cex=par()$cex.lab)
+plot(x=25:100, ylim=c(1,4.5), axes=FALSE, ann=FALSE, xlim=c(25,100))
+lcomp5 <- ss3sim:::get_args("cases/lcomp5-cod.txt")
+with(lcomp5, points(years[[1]], y=rep(2, len=length(years[[1]])), cex=cex.fishery))
+with(lcomp5, points(years[[2]], y=rep(2.25, len=length(years[[2]])), cex=cex.survey, pch=16))
+index5 <- ss3sim:::get_args("cases/index5-cod.txt")
+with(index5, points(years[[1]], y=rep(3, len=length(years[[1]])), cex=cex.fishery))
+agecomp5 <- ss3sim:::get_args("cases/agecomp5-cod.txt")
+with(agecomp5, points(years[[1]], y=rep(1, len=length(years[[1]])), cex=cex.fishery))
+with(agecomp5, points(years[[2]], y=rep(1.25, len=length(years[[2]])), cex=cex.survey, pch=16))
+points(x=25:100, y=rep(4, len=76), cex=cex.fishery)
+axis(1, at=seq(25,100, by=25), col=col.border, mgp=par()$mgp, tck=par()$tck)
+text(x=90, y=c(1.5,2.5,3.25,4.25), labels=c("Lengths", "Ages", "Index", "Catches"))
+box(col=col.border)
+mtext("Year", side=1, line=1,cex=par()$cex.lab)
 dev.off()
 
-####
-df <- read.csv("results/Binning_lit_review_10_2014.csv")
-str(df)
-g <- ggplot(data=df, aes(x=Linf_cm,
-            col=Internal_External))+facet_wrap("Region") + xlab("Linf") +
-    scale_color_discrete(name="Growth Estimation",
-                         labels=c("Not yet specified", "Empirical",
-                         "External", "Internal"))
-g2 <- g+geom_point(aes(y=num_length_bin))+ylab("Number of bins")
-ggsave("plots/lit_review_num_bins.png", g2, width=7, height=4.5)
-g2 <- g+geom_point(aes(y=bin_width_cm))+ylab("Width of bins (cm)") + ylim(0,10.5)
-ggsave("plots/lit_review_width_bins.png", g2, width=7, height=4.5)
-
-### ------------------------------------------------------------
 
 
-make_selex_plot <- function(B){
-    species <- c('cod', 'flatfish', 'yellow')
-    scenarios <- expand_scenarios(cases=list(D=2, E=991, F=1, I=0, B=B), species=species)
-    circle.scalar <- 1
-    par(mfrow=c(1,3), mgp=c(2,.5,0), mar=c(3,3,3,1))
-    for(scenario in scenarios){
-        fleet <- 1
-        replist <- r4ss::SS_output(file.path("example_models", scenario, "1", "om/"), covar=F, forecast=F,
-                                   ncols=300, readwt=FALSE, printstats=FALSE, verbose=FALSE)
-        dat <- SS_readdat(file.path("example_models", scenario, "1", "em/ss3.dat"), verbose=FALSE)
-        lbins <- dat$lbin_vector
-        agebins <- dat$agebin_vector
-        ## Get length samples aggegrated across year for specified fleet
-        lbin.proportions <- as.vector(colSums(dat$lencomp[dat$lencomp[,3]==fleet,-(1:6)]))
-        lbin.proportions <- lbin.proportions/max(lbin.proportions)*circle.scalar
-        agebin.proportions <- as.vector(colSums(dat$agecomp[dat$agecomp[,3]==fleet,-(1:9)]))
-        agebin.proportions <- agebin.proportions/max(agebin.proportions)*circle.scalar
-        selexinfo <- SSplotSelex(replist = replist, selexlines = 1:6, subplot=21,
-                                 fleets = fleet , fleetnames = "default", plot = TRUE,
-                                 print = FALSE,  pwidth = 6.5, pheight = 5,
-                                 punits = "in", ptsize = 10, showmain=FALSE)
-        abline(h=lbins, col=gray(.8), lty=2)
-        mtext(scenario, line=-2, cex=.75)
-        symbols(x=rep(max(agebins), length(lbins)), y=lbins, circles=lbin.proportions, add=TRUE,
-                inches=FALSE, bg='blue')
-        symbols(y=rep(min(lbins), length(agebins)), x=agebins, circles=agebin.proportions, add=TRUE,
-                inches=FALSE, bg='green')
-    }
-}
 
-for(bb in 0:4){
-png(paste0('plots/selex_B', bb, '.png'), width=9, height=5, units='in', res=300)
-make_selex_plot(bb)
-dev.off()
-}
 
-## ## default args from function -- could play with these to clean up the
-## ## plots if need be
-## function (replist, infotable = NULL, fleets = "all", fleetnames = "default",
-##     sizefactors = c("Lsel"), agefactors = c("Asel", "Asel2"),
-##     years = "endyr", season = 1, sexes = "all", selexlines = 1:6,
-##     subplot = 1:25, skipAgeSelex10 = TRUE, plot = TRUE, print = FALSE,
-##     add = FALSE, labels = c("Length (cm)", "Age (yr)", "Year",
-##         "Selectivity", "Retention", "Discard mortality"), col1 = "red",
-##     col2 = "blue", lwd = 2, fleetcols = "default", fleetpch = "default",
-##     fleetlty = "default", spacepoints = 5, staggerpoints = 1,
-##     legendloc = "bottomright", pwidth = 7, pheight = 7, punits = "in",
-##     res = 300, ptsize = 12, cex.main = 1, showmain = TRUE, plotdir = "default",
-##     verbose = TRUE)
+
+## for(bb in 0:4){
+## png(paste0('plots/selex_B', bb, '.png'), width=9, height=5, units='in', res=300)
+## make_selex_plot(bb)
+## dev.off()
+## }
+## ## ## default args from function -- could play with these to clean up the
+## ## ## plots if need be
+## ## function (replist, infotable = NULL, fleets = "all", fleetnames = "default",
+## ##     sizefactors = c("Lsel"), agefactors = c("Asel", "Asel2"),
+## ##     years = "endyr", season = 1, sexes = "all", selexlines = 1:6,
+## ##     subplot = 1:25, skipAgeSelex10 = TRUE, plot = TRUE, print = FALSE,
+## ##     add = FALSE, labels = c("Length (cm)", "Age (yr)", "Year",
+## ##         "Selectivity", "Retention", "Discard mortality"), col1 = "red",
+## ##     col2 = "blue", lwd = 2, fleetcols = "default", fleetpch = "default",
+## ##     fleetlty = "default", spacepoints = 5, staggerpoints = 1,
+## ##     legendloc = "bottomright", pwidth = 7, pheight = 7, punits = "in",
+## ##     res = 300, ptsize = 12, cex.main = 1, showmain = TRUE, plotdir = "default",
+## ##     verbose = TRUE)
+## make_selex_plot <- function(B){
+##     species <- c('cod', 'flatfish', 'yellow')
+##     scenarios <- expand_scenarios(cases=list(D=2, E=991, F=1, I=0, B=B), species=species)
+##     circle.scalar <- 1
+##     par(mfrow=c(1,3), mgp=c(2,.5,0), mar=c(3,3,3,1))
+##     for(scenario in scenarios){
+##         fleet <- 1
+##         replist <- r4ss::SS_output(file.path("example_models", scenario, "1", "om/"), covar=F, forecast=F,
+##                                    ncols=300, readwt=FALSE, printstats=FALSE, verbose=FALSE)
+##         dat <- SS_readdat(file.path("example_models", scenario, "1", "em/ss3.dat"), verbose=FALSE)
+##         lbins <- dat$lbin_vector
+##         agebins <- dat$agebin_vector
+##         ## Get length samples aggegrated across year for specified fleet
+##         lbin.proportions <- as.vector(colSums(dat$lencomp[dat$lencomp[,3]==fleet,-(1:6)]))
+##         lbin.proportions <- lbin.proportions/max(lbin.proportions)*circle.scalar
+##         agebin.proportions <- as.vector(colSums(dat$agecomp[dat$agecomp[,3]==fleet,-(1:9)]))
+##         agebin.proportions <- agebin.proportions/max(agebin.proportions)*circle.scalar
+##         selexinfo <- SSplotSelex(replist = replist, selexlines = 1:6, subplot=21,
+##                                  fleets = fleet , fleetnames = "default", plot = TRUE,
+##                                  print = FALSE,  pwidth = 6.5, pheight = 5,
+##                                  punits = "in", ptsize = 10, showmain=FALSE)
+##         abline(h=lbins, col=gray(.8), lty=2)
+##         mtext(scenario, line=-2, cex=.75)
+##         symbols(x=rep(max(agebins), length(lbins)), y=lbins, circles=lbin.proportions, add=TRUE,
+##                 inches=FALSE, bg='blue')
+##         symbols(y=rep(min(lbins), length(agebins)), x=agebins, circles=agebin.proportions, add=TRUE,
+##                 inches=FALSE, bg='green')
+##     }
+## }
