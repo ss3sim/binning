3b109dfc86fc8afdf544ce862261f9adddbd6f68
colemonnahan <monnahc@uw.edu>
2015-04-06 11:15:38 -0700

    small update to get ready to run

--- run_rbtc_tests.R
+++ run_rbtc_tests.R
@@ -7,16 +7,15 @@ Nsim <- Nsim.datarich
 for(spp in species){
     scenarios <- expand_scenarios(cases=list(D=c(2,3), F=1, I=11:14, B=0), species=spp)
     run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-               parallel=TRUE, parallel_iterations=FALSE,
+               parallel=TRUE, parallel_iterations=TRUE,
                case_folder=case_folder, om_dir=ss3model(spp, "om"),
-               admb_options=" -maxfn 1 -phase 50",
                em_dir=ss3model(spp, "em"), case_files=case_files)
 }
 Nsim <- Nsim.datapoor
 for(spp in species){
     scenarios <- expand_scenarios(cases=list(D=c(5,6), F=1, I=11:14, B=0), species=spp)
     run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-               parallel=TRUE, parallel_iterations=FALSE,
+               parallel=TRUE, parallel_iterations=TRUE,
                case_folder=case_folder, om_dir=ss3model(spp, "om"),
                em_dir=ss3model(spp, "em"), case_files=case_files)
 }
@@ -33,7 +32,7 @@ Nsim <- Nsim.datarich
 for(spp in species){
     scenarios <- expand_scenarios(cases=list(D=c(2,3), F=1, I=21:24, B=0), species=spp)
     run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-               parallel=TRUE, parallel_iterations=FALSE,
+               parallel=TRUE, parallel_iterations=TRUE,
                case_folder=case_folder, om_dir=ss3model(spp, "om"),
                em_dir=ss3model(spp, "em"), case_files=case_files)
 }
@@ -41,7 +40,7 @@ Nsim <- Nsim.datapoor
 for(spp in species){
     scenarios <- expand_scenarios(cases=list(D=c(5,6), F=1, I=21:24, B=0), species=spp)
     run_ss3sim(iterations=1:Nsim, scenarios=scenarios,
-               parallel=TRUE, parallel_iterations=FALSE,
+               parallel=TRUE, parallel_iterations=TRUE,
                case_folder=case_folder, om_dir=ss3model(spp, "om"),
                em_dir=ss3model(spp, "em"), case_files=case_files)
 }
