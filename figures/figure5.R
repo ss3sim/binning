c98787e9c5b9b83975f9dbc5080ea028d00b0ee7
colemonnahan <monnahc@uw.edu>
2015-04-07 17:51:18 -0700

    tweaks to fig 5

--- figures/figure5.R
+++ figures/figure5.R
@@ -22,13 +22,14 @@ make.file(file.type, filename='figures/figure5_tradeoffs.png',
           res=500, width=width, height=4)
 par(mar=c(.1,.1,.1,.1), tck=-0.03, oma=c(2.5,3,1.5,.5),
     mgp=c(.5, .2,0), mfrow=c(3,7), cex.lab=.8, cex.axis=.75,
-    col.axis=col.label, xpd=FALSE)
+    col.axis=col.label, xpd=FALSE, title.cex=.7)
 myvariables <- c("CV_young", "CV_old", "L_at_Amin", "L_at_Amax",
                  "VonBert_K", "SSB_MSY", "depletion")
 myvariables.labels <- c(expression(CV[young]), expression(CV[old]),
                         expression(L[min]), expression(L[infinity]),
                         expression(italic(k)), expression(SSB[MSY]),
                         "Depletion")
+myspecies <- species
 species.labels <- c("cod", "flatfish", "rockfish")
 xy <- c(.1, .1); k <- 1
 myletters <- c(letters, "aa", "ab")
@@ -36,11 +37,16 @@ for(i in seq_along(species)){
     for(j in seq_along(myvariables)){
         d.temp <- d[d$species==species[i] & d$variable==myvariables[j],]
         d.temp <- subset(d, species==myspecies[i] & variable==myvariables[j])
+        pch.cex <- .5*seq_along(d.temp$binwidth)
         plot(0,0, type='n', xlim=c(0,5.5), ylim=c(-.25,.25), axes=FALSE, ann=FALSE)
         abline(h=0, col=col.border)
         ## only make plots if had enough converged iterations
-        with(d.temp, points(x=median.runtime, y=MRE, pch=16, cex=.5*seq_along(binwidth), col=rgb(0,0,0,.5)))
+        with(d.temp, points(x=median.runtime, y=MRE, pch=16, cex=pch.cex, col=rgb(0,0,0,.5)))
         print.letter(paste0("(", myletters[k], ")"), xy=xy, cex=.7); k <- k+1
+        if( i==1 & j==1)
+            legend("top", legend=c(1,2,5,10,20), pch=16, pt.cex=pch.cex,
+                   bty='n',horiz=TRUE, cex=.5, adj=0, xjust=3, col=rgb(0,0,0,.5),
+                   title='Bin Width (cm)')
         if(i==1) mtext(myvariables.labels[j], side=3, line=0, cex=par()$cex.lab)
         if(i==length(species))
             axis(1, col=col.border, mgp=par()$mgp, tck=par()$tck)
