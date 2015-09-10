25a2388feca76b9f444a119460ae51ca3d55b739
colemonnahan <monnahc@uw.edu>
2015-09-10 16:25:34 -0700

    Add table of MAREs

--- make_tables.R
+++ make_tables.R
@@ -1,6 +1,23 @@
 ## Source thsi file to make the tables, note it creates csv files in the
 ## results folder
 
+## table of MARES since now dropped from the paper
+mare.table.long <- ddply(binning.long.figure, .(species,data,binmatch,variable, data.binwidth),
+           summarize,
+            median_ = median(value, na.rm = TRUE),
+            l2      = quantile(value, 0.25, na.rm = TRUE),
+            u2      = quantile(value, 0.75, na.rm = TRUE),
+           mare    = 100 * round(median(abs(value), na.rm = TRUE), 3),
+           pct.converged=pct.converged[1],
+           count = length(value))
+## cast it into wide for easier reading
+mare.table.long$binmatch <- ifelse(mare.table.long$binmatch=='pbin=1cm', 'Match', 'No Match')
+str(mare.table.long)
+mare.table.wide <- reshape2::dcast(mare.table.long,
+                              formula=species+data+variable+binmatch~data.binwidth,
+                              value.var='mare')
+write.csv(mare.table.wide, 'results/table_mares.csv')
+
 ## table of converged binning scenarios
 write.table(dcast(binning.counts, value.var='pct.converged',
                 formula=species+data~B),
@@ -32,36 +49,36 @@ write.table(x=binning.management.table,
             file='results/table_binning_management.csv', sep=',',
             row.names=FALSE)
 
-tcomp.growth.table <- dcast(ddply(tcomp.long.growth, .(species, data, tvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
-write.table(x=tcomp.growth.table,
-            file='results/table_tcomp_growth.csv', sep=',',
-            row.names=FALSE)
-tcomp.selex.table <- dcast(ddply(tcomp.long.selex, .(species, data, tvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
-write.table(x=tcomp.selex.table,
-            file='results/table_tcomp_selex.csv', sep=',',
-            row.names=FALSE)
-tcomp.management.table <- dcast(ddply(tcomp.long.management, .(species, data, tvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
-write.table(x=tcomp.management.table,
-            file='results/table_tcomp_management.csv', sep=',',
-            row.names=FALSE)
+## tcomp.growth.table <- dcast(ddply(tcomp.long.growth, .(species, data, tvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
+## write.table(x=tcomp.growth.table,
+##             file='results/table_tcomp_growth.csv', sep=',',
+##             row.names=FALSE)
+## tcomp.selex.table <- dcast(ddply(tcomp.long.selex, .(species, data, tvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
+## write.table(x=tcomp.selex.table,
+##             file='results/table_tcomp_selex.csv', sep=',',
+##             row.names=FALSE)
+## tcomp.management.table <- dcast(ddply(tcomp.long.management, .(species, data, tvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~tvalue)
+## write.table(x=tcomp.management.table,
+##             file='results/table_tcomp_management.csv', sep=',',
+##             row.names=FALSE)
 
 
-robust.growth.table <- dcast(ddply(robust.long.growth, .(species, data, rvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
-write.table(x=robust.growth.table,
-            file='results/table_robust_growth.csv', sep=',',
-            row.names=FALSE)
-robust.selex.table <- dcast(ddply(robust.long.selex, .(species, data, rvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
-write.table(x=robust.selex.table,
-            file='results/table_robust_selex.csv', sep=',',
-            row.names=FALSE)
-robust.management.table <- dcast(ddply(robust.long.management, .(species, data, rvalue, variable), summarize,
-      median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
-write.table(x=robust.management.table,
-            file='results/table_robust_management.csv', sep=',',
-            row.names=FALSE)
+## robust.growth.table <- dcast(ddply(robust.long.growth, .(species, data, rvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
+## write.table(x=robust.growth.table,
+##             file='results/table_robust_growth.csv', sep=',',
+##             row.names=FALSE)
+## robust.selex.table <- dcast(ddply(robust.long.selex, .(species, data, rvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
+## write.table(x=robust.selex.table,
+##             file='results/table_robust_selex.csv', sep=',',
+##             row.names=FALSE)
+## robust.management.table <- dcast(ddply(robust.long.management, .(species, data, rvalue, variable), summarize,
+##       median.re=round(median(value),3)), value.var="median.re", formula=species+data+variable~rvalue)
+## write.table(x=robust.management.table,
+##             file='results/table_robust_management.csv', sep=',',
+##             row.names=FALSE)
 
